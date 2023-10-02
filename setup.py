ff67fea0ba5f102cf4cc630fdde9226a7698433a
Alec Edgington <alec.edgington@quantinuum.com>
2023-10-02 16:22:28 +0100

    Update to pytket 1.20.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.19",
+        "pytket ~= 1.20",
         "qsharp ~= 0.28.291394",
         "qsharp-core ~= 0.28.291394",
         "markdown",
