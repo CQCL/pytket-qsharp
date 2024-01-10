97b5c867d84b66ef353d3045253698c2818a33d2
Melf <cqc@melf.de>
2024-01-10 14:52:58 +0000

    update pytket version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.22",
+        "pytket ~= 1.23",
         "qsharp ~= 0.28.291394",
         "qsharp-core ~= 0.28.291394",
         "markdown",
